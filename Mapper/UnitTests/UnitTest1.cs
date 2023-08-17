b93708a618e0a7e197dbf5748df509d7ff95fb23
aromic <ante_ilok@hotmail.com>
2023-08-17 19:24:20 +0200

    Unit tests setup added

--- Mapper/UnitTests/UnitTest1.cs
+++ Mapper/UnitTests/UnitTest1.cs
@@ -1,24 +1,10 @@
-﻿using Configuration;
 using Infrastructure;
 using Mappings;
-using Models;
-using NUnit.Framework;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Reflection;
-using System.Reflection.Emit;
-using System.Xml;
 
-namespace Program
+namespace UnitTests
 {
-    [TestFixture]
-    public class Program
+    public class Tests
     {
-
-        public Program() { }
 
         public interface IShape
         {
@@ -26,7 +12,7 @@ namespace Program
             public bool IsGeometryShape { get; set; }
         }
 
-        public class Shape :IShape
+        public class Shape : IShape
         {
             public bool IsGeometryShape { get; set; }
         }
@@ -53,7 +39,7 @@ namespace Program
             public bool IsGeometryShape { get; set; }
         }
 
-        public class Drawing :IDrawing
+        public class Drawing : IDrawing
         {
             public Drawing(IDrawing? parent = null)
             {
@@ -69,8 +55,9 @@ namespace Program
         public interface IDrawing
         {
             public IShape MainShape { get; set; }
-        
+
             public IEnumerable<ILine> Lines { get; set; }
+
             public IDrawing Parent { get; set; }
         }
 
@@ -137,14 +124,24 @@ namespace Program
             var drawing = new Drawing { MainShape = new Shape { IsGeometryShape = true }, Lines = new[] { new Line { Start = 0, End = 1, Name = "Line1" } } };
             var lines = new List<LineRest>();
             lines.Add(new LineRest { Start = 3, End = 4 });
-            var drawingRest = new DrawingRest { MainShape = new ShapeRest { IsGeometryShape = false }};
-            //drawingRest.Lines = lines; if we uncomment this line mapper should map lines to drawing.Lines,otherwise it's expected for mapper to set Lines to null on drawing in the next line
+            var drawingRest = new DrawingRest { MainShape = new ShapeRest { IsGeometryShape = false } };
+            drawingRest.Lines = lines; //if we uncomment this line mapper should map lines to drawing.Lines,otherwise it's expected for mapper to set Lines to null on drawing in the next line
             //When mapping configurations are implemented, it will be possible to set lines to be ignored on destination so they are never overriden to empty array if we don't want it.
             mapper.Map(drawingRest, drawing);
             IDrawing newDrawing = mapper.Map<IDrawing>(drawingRest); //we should get newDrawing as a new object with all the properties from IDrawing and values set to the properties drawingRest has.
             IEnumerable<IDrawing> drawings = mapper.Map<IEnumerable<IDrawing>>(new[] { drawingRest }); //as above but drawings should be a new IEnumerable<IDrawing> with the same amount of items as the source value set.
+            Assert.That(drawingRest.MainShape.IsGeometryShape, Is.EqualTo(newDrawing.MainShape.IsGeometryShape));
+            Assert.That(drawingRest.Lines.First().Start, Is.EqualTo(newDrawing.Lines.First().Start));
+            Assert.That(drawingRest.Lines.First().End, Is.EqualTo(newDrawing.Lines.First().End));
+            Assert.That(drawingRest.Lines.First().Name, Is.EqualTo(newDrawing.Lines.First().Name));
+            Assert.That(drawingRest.MainShape.IsGeometryShape, Is.EqualTo(drawing.MainShape.IsGeometryShape));
+            Assert.That(drawingRest.Lines.First().Start, Is.EqualTo(drawing.Lines.First().Start));
+            Assert.That(drawingRest.Lines.First().End, Is.EqualTo(drawing.Lines.First().End));
+            Assert.That(drawingRest.Lines.First().Name, Is.EqualTo(drawing.Lines.First().Name));
+            Assert.That(drawingRest.MainShape.IsGeometryShape, Is.EqualTo(drawings.First().MainShape.IsGeometryShape));
+            Assert.That(drawingRest.Lines.First().Start, Is.EqualTo(drawings.First().Lines.First().Start));
+            Assert.That(drawingRest.Lines.First().End, Is.EqualTo(drawings.First().Lines.First().End));
+            Assert.That(drawingRest.Lines.First().Name, Is.EqualTo(drawings.First().Lines.First().Name));
         }
     }
-
 }
-
